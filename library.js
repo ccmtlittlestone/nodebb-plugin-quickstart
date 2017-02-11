4b7e1f8cf2b28d05794f67fdba37eddd693b095f
phyer@sina.com <phyer@sina.com>
2017-02-12 01:56:46 +0800

    change route

--- library.js
+++ library.js
@@ -14,8 +14,8 @@ plugin.init = function(params, callback) {
 
 	router.get('/admin/plugins/quickstart', hostMiddleware.admin.buildHeader, controllers.renderAdminPage);
 	router.get('/api/admin/plugins/quickstart', controllers.renderAdminPage);
-	router.get('/api/plugins/quickstart', controllers.renderPage);
 	router.get('/api/plugins/quickstart/categories', controllers.renderPage);
+	router.get('/api/plugins/quickstart/', controllers.renderPage);
 
 	callback();
 };
